c7d36efd2ec76938c62825c0866182aa3b6c8485
arfan36 <arfanur36@gmail.com>
2022-08-05 16:42:06 +0600

    19-4 Everything you need...

--- 4_return.js
+++ 4_return.js
@@ -1,9 +1,18 @@
 function add(number1, number2) {
-    console.log(number1, number2);
+    // console.log(number1, number2);
     var sum = number1 + number2;
     // console.log(sum);
     return sum;
 }
 // add(45, 15);
 var total = add(80, 20);
-console.log('total', total);
+// console.log('total', total);
+
+function bringSingra(money) {
+    var singraPrice = 10;
+    var quantity = money / singraPrice;
+    return quantity;
+}
+var myTaka = 150;
+var singras = bringSingra(myTaka);
+console.log(singras);
