213eb10f7ca303de85fc6d4626b4fbaedaaa894f
arfan36 <arfanur36@gmail.com>
2022-08-05 20:28:37 +0600

    19-7 multiple ways to ...

---
+++ 7_properties.js
@@ -0,0 +1,30 @@
+var shoppingCart = {
+    books: 3,
+    sunglass: 1,
+    keyboard: 5,
+    mouse: 1,
+    pen: 25
+};
+// when you know the specific property name, use dot notation to get the value
+var penCount = shoppingCart.pen;
+// alternative System
+// when you know the specific property name, use dot notation to get the value
+var penCount2 = shoppingCart['pen'];
+
+var propertiesName = 'mouse';
+var propertiesValue = shoppingCart[propertiesName];
+// console.log(propertiesName, propertiesValue);
+
+var properties = Object.keys(shoppingCart);
+var propertiesValue = Object.values(shoppingCart);
+// console.log(properties);
+// console.log(propertiesValue);
+console.log(shoppingCart);
+
+// set property values
+shoppingCart.mouse = 15;
+console.log(shoppingCart);
+shoppingCart['mouse'] = 29;
+console.log(shoppingCart);
+shoppingCart[propertiesName] = 89;
+console.log(shoppingCart);
