c279fc61ffde8d0302f55c120cae4f3b1cd8cf0d
arfan36 <arfanur36@gmail.com>
2022-08-05 22:10:41 +0600

    19-9 Module summary...

---
+++ 9_switch.js
@@ -0,0 +1,40 @@
+const color = 'yellow';
+if (color === 'green') {
+    console.log('You are green friend');
+}
+else if (color === 'blue') {
+    console.log('You are blue friend');
+}
+else if (color === 'red') {
+    console.log('You are red friend');
+}
+else if (color === 'white') {
+    console.log('You are white friend');
+}
+else if (color === 'yellow') {
+    console.log('You are yellow friend');
+}
+else {
+    console.log('You are black friend');
+}
+
+// switch
+switch (color) {
+    case 'green':
+        console.log('You are a green friend');
+        break;
+    case 'blue':
+        console.log('You are a blue friend');
+        break;
+    case 'white':
+        console.log('You are a white friend');
+        break;
+    case 'red':
+        console.log('You are a red friend');
+        break;
+    case 'yellow':
+        console.log('You are a yellow friend');
+        break;
+    default:
+        console.log('You are a kala kala friend');
+}
