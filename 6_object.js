846ad81fa5a6bf0ebbab59824e61541caec378d2
arfan36 <arfanur36@gmail.com>
2022-08-05 18:51:35 +0600

    19-6 Declare multiple...

---
+++ 6_object.js
@@ -0,0 +1,24 @@
+var student = {
+    id: 115,
+    name: 'Solamin Khan',
+    class: 9,
+    marks: 98
+};
+
+var mobile = {
+    brand: 'Samsung',
+    price: 19000,
+    stroge: '65gb',
+    camera: '5MP'
+};
+
+
+var myComputer = {
+    brand: 'lenovo',
+    price: 39000,
+    color: 'silver',
+    processor: 'i7'
+};
+console.log(myComputer);
+myComputer.processor = 'i79';
+console.log(myComputer);
